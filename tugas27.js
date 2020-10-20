edc1f2b3095c25538e1e11ab8ee524fdb38c8652
ayuberlianda <71421525+ayuberlianda@users.noreply.github.com>
2020-10-20 22:44:04 +0700

    Add files via upload

---
+++ tugas27.js
@@ -0,0 +1,11 @@
+var alamat1 = ["No","rumah saya", 30,"A"]
+var alamat2 = ["Jalan pangeran Tirtayasa No", 70]
+var strAlamat1 = "", strAlamat2 = ""
+
+for(var i = 0; i<alamat1.length; i++){
+	strAlamat1 = strAlamat1.concat(alamat1[i]," ");
+}
+
+for(var i = 0; i<alamat2.length; i++){
+	strAlamat2 = strAlamat2.concat( alamat2[i]," ");
+}
